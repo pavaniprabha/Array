6efbf3470ff92b45a534270fef35957b5fb736cb
RAGOLUpavani <ragolupavani0330@gmail.com>
2021-10-07 12:56:39 +0530

    arrays concepts are committed

---
+++ src/com/blz/arraylearning/ArrayDemo.java
@@ -0,0 +1,32 @@
+package com.blz.arraylearning;
+
+import java.util.Scanner;
+
+public class ArrayDemo {
+	 private static final Scanner sc = new Scanner(System.in);
+     public static void main(String[] args) {
+//    	System.out.println("enter size of array :");
+//		int limit = sc.nextInt();
+//    	 int[] intArray =  new int[limit];
+    	 int[] Array1 = {1,2,3,4,5,6,7,8};
+    	 Array1[7] = 68;
+//    	 
+//      	 for (int i = 0; i< intArray.length; i++) {
+//        System.out.println("enter " + i + "indexvalue :\n" );
+//    	  intArray[i] = sc.nextInt();
+//    	 }
+//        for(int i=0;i<intArray.length;i++) {
+//        System.out.println(intArray[i] + " " );
+   
+      
+//        
+//     	 for (int i = 0; i< Array1.length; i++) {
+//       System.out.println("enter " + i + "indexvalue :\n" );
+//   	  Array1[i] = sc.nextInt();
+//   	 }
+       for(int i=0;i<Array1.length;i++) {
+       System.out.println(Array1[i] + " " );
+        
+        }
+}}
+        
